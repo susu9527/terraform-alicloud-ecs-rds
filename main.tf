2bd6ba7343af66628fe9030f78c1818c1630ef23
susu9527 <73459029+susu9527@users.noreply.github.com>
2021-12-27 16:46:30 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "alicloud_instance" "default" {
+  instance_name              = var.name
+  availability_zone          = var.availability_zone
+  security_groups            = var.security_group_ids
+  vswitch_id                 = var.vswitch_id
+  instance_type              = var.instance_type
+  system_disk_category       = var.system_disk_category
+  system_disk_name           = var.system_disk_name
+  system_disk_description    = var.system_disk_description
+  image_id                   = var.image_id
+  internet_max_bandwidth_out = var.internet_max_bandwidth_out
+  data_disks {
+    name        = var.name
+    size        = var.ecs_size
+    category    = var.category
+    description = var.description
+    encrypted   = true
+  }
+}
+
+resource "alicloud_db_instance" "default" {
+  instance_name        = var.name
+  vswitch_id           = var.vswitch_id
+  engine               = "MySQL"
+  engine_version       = var.engine_version
+  instance_type        = var.rds_instance_type
+  instance_storage     = var.instance_storage
+  instance_charge_type = var.instance_charge_type
+  monitoring_period    = var.monitoring_period
+}
